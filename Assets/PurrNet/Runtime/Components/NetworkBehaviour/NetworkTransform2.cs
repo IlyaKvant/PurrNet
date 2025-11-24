0a14fba3a935ee51e4d24b29046eee9906ede76a
BlenMiner <blenminer123@gmail.com>
2025-11-24 10:57:16 +0100

    boiler plate and brainstorming

---
+++ Assets/PurrNet/Runtime/Components/NetworkBehaviour/NetworkTransform2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using Vector3 = System.Numerics.Vector3;
+
+namespace PurrNet
+{
+    [Serializable]
+    public struct Resolution<T>
+    {
+        public bool uniformResoltion;
+        public T perComponentResolution;
+    }
+
+    [Serializable]
+    public struct TickRate
+    {
+        public bool managerDefault;
+        public int customRate;
+    }
+
+    [Serializable]
+    public struct NetworkTransformSettings
+    {
+        [Header("Rate of sync")]
+        public TickRate tickRate;
+        [Header("What to sync")]
+        public SyncMode syncPosition;
+        public SyncMode syncRotation;
+        public bool syncScale;
+        public bool syncParent;
+        [Header("Precision settings")]
+        public Resolution<Vector3> positionResolution;
+        public Resolution<Vector3> rotationResolution;
+        public Resolution<Vector3> scaleResolution;
+    }
+
+    public struct NetworkTransformState
+    {
+
+    }
+
+    public class NetworkTransform2 : NetworkIdentity
+    {
+        private void OnEnable()
+        {
+            UnityLatestUpdate.onLatestUpdate += LatestUpdate;
+        }
+
+        private void OnDisable()
+        {
+            UnityLatestUpdate.onLatestUpdate -= LatestUpdate;
+        }
+
+        private void LatestUpdate()
+        {
+
+        }
+    }
+}
